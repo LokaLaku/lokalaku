0e5e4097dc3f786d0dd234db92527b456b9860c1
LokaLaku <lokalakutrip@gmail.com>
2025-08-09 17:38:04 +0700

    Add files via upload

--- js/toucheffects.js
+++ js/toucheffects.js
@@ -1,1 +1,81 @@
-!function(e){if(Modernizr.touch){function n(e){return new RegExp("(^|\\s+)"+e+"(\\s+|$)")}var t,s,a;function c(e,n){(t(e,n)?a:s)(e,n)}"classList"in document.documentElement?(t=function(e,n){return e.classList.contains(n)},s=function(e,n){e.classList.add(n)},a=function(e,n){e.classList.remove(n)}):(t=function(e,t){return n(t).test(e.className)},s=function(e,n){t(e,n)||(e.className=e.className+" "+n)},a=function(e,t){e.className=e.className.replace(n(t)," ")});var o={hasClass:t,addClass:s,removeClass:a,toggleClass:c,has:t,add:s,remove:a,toggle:c};"function"==typeof define&&define.amd?define(o):e.classie=o,[].slice.call(document.querySelectorAll("ul.grid > li > figure")).forEach(function(e,n){e.querySelector("figcaption > a").addEventListener("touchstart",function(e){e.stopPropagation()},!1),e.addEventListener("touchstart",function(e){o.toggle(this,"cs-hover")},!1)})}}(window);
+/** Used Only For Touch Devices **/
+( function( window ) {
+	
+	// for touch devices: add class cs-hover to the figures when touching the items
+	if( Modernizr.touch ) {
+
+		// classie.js https://github.com/desandro/classie/blob/master/classie.js
+		// class helper functions from bonzo https://github.com/ded/bonzo
+
+		function classReg( className ) {
+			return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+		}
+
+		// classList support for class management
+		// altho to be fair, the api sucks because it won't accept multiple classes at once
+		var hasClass, addClass, removeClass;
+
+		if ( 'classList' in document.documentElement ) {
+			hasClass = function( elem, c ) {
+				return elem.classList.contains( c );
+			};
+			addClass = function( elem, c ) {
+				elem.classList.add( c );
+			};
+			removeClass = function( elem, c ) {
+				elem.classList.remove( c );
+			};
+		}
+		else {
+			hasClass = function( elem, c ) {
+				return classReg( c ).test( elem.className );
+			};
+			addClass = function( elem, c ) {
+				if ( !hasClass( elem, c ) ) {
+						elem.className = elem.className + ' ' + c;
+				}
+			};
+			removeClass = function( elem, c ) {
+				elem.className = elem.className.replace( classReg( c ), ' ' );
+			};
+		}
+
+		function toggleClass( elem, c ) {
+			var fn = hasClass( elem, c ) ? removeClass : addClass;
+			fn( elem, c );
+		}
+
+		var classie = {
+			// full names
+			hasClass: hasClass,
+			addClass: addClass,
+			removeClass: removeClass,
+			toggleClass: toggleClass,
+			// short names
+			has: hasClass,
+			add: addClass,
+			remove: removeClass,
+			toggle: toggleClass
+		};
+
+		// transport
+		if ( typeof define === 'function' && define.amd ) {
+			// AMD
+			define( classie );
+		} else {
+			// browser global
+			window.classie = classie;
+		}
+
+		[].slice.call( document.querySelectorAll( 'ul.grid > li > figure' ) ).forEach( function( el, i ) {
+			el.querySelector( 'figcaption > a' ).addEventListener( 'touchstart', function(e) {
+				e.stopPropagation();
+			}, false );
+			el.addEventListener( 'touchstart', function(e) {
+				classie.toggle( this, 'cs-hover' );
+			}, false );
+		} );
+
+	}
+
+})( window );
